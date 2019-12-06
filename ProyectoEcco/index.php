a53c919ee17c250451bc9c220e99aec64ed190db
beajg <54404109+beajg@users.noreply.github.com>
2019-12-06 00:36:47 -0600

    Create index.php

---
+++ ProyectoEcco/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="Background credits" content="All of our pictures are copyleft - Photo by Blake Cheek on Unsplash, Photo by Claudio Testa on Unsplash, Photo by Justin Kauffman on Unsplash, Photo by adrian korpal on Unsplash">
+    <title>Eccopoli</title>
+    <link rel = "stylesheet" href = "style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Heebo|Source+Sans+Pro&display=swap" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    
+  
+</head>
+<body>
+    <nav role="navigation">
+    <div id="menuToggle"> 
+        
+    <input type="checkbox" /> 
+    <span></span>
+    <span></span>
+    <span></span> 
+    <h4>MENU</h4>    
+    <ul id="menu">
+            <a href="Index.html"><li>Eccopoli</li></a>
+            <a href="pilares.html"><li>Pilares</li></a>
+            <a href="equipo.html"><li>Equípo</li></a>
+            <a href="contacto.html"><li>Contacto</li></a>
+            <a href="galeria.html"><li>Galería</li></a>
+            <a href="mision.html"><li>Misión</li></a>
+          </ul>
+        </div>
+      </nav> 
+<section class="background">
+ <section class="cabecera">
+  <div class="eco">ECCOPOLI</div>    
+  <div class="logo"></div>
+ </section>  
+
+ <div class="foot"><p> Derechos reservados a Eccopoli© 2019 </p></div>
+</section>
+
+
+</body>
+</html>
